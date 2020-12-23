97b676fa3691043a7cbfe9ed4200f91595168fe5
DocWeirdo <corin.baurmann@gmail.com>
2020-12-23 23:29:47 +0100

    continued rework

--- lib/states/AppState.dart
+++ lib/states/AppState.dart
@@ -13,33 +13,35 @@ class AppState extends ChangeNotifier {
 
   UserModel _user;
 
-  /// Updates the [AppState] and containing [UserModel] when the Firebase User has changed.
+  /// Updates the [AppState] and contained [UserModel] when the Firebase User has changed.
   void update(FirebaseUser authUser) async {
     UserModel user = await DataService.getUserModel(authUser.uid, selectedPool);
 
-    if (user == null) {
-      // Incase a new user is created and the cloudfunction hasn't fired yet
-      _user = UserModel(
-          authUser.uid,
-          authUser.isAnonymous ? 'Anonymous' : authUser.displayName,
-          authUser.photoUrl,
-          identifier: authUser.isAnonymous
-              ? null
-              : (authUser.email ?? authUser.phoneNumber),
-          isAnonymous: authUser.isAnonymous);
-    } else
-      _user = user;
+    // Incase a new user is created and the cloudfunction hasn't fired yet
+    user ??= UserModel(
+        authUser.uid,
+        authUser.isAnonymous ? 'Anonymous' : authUser.displayName,
+        authUser.photoUrl,
+        identifier: authUser.isAnonymous
+            ? null
+            : (authUser.email ?? authUser.phoneNumber),
+        isAnonymous: authUser.isAnonymous);
 
-    DataService.updatedUserModel(_user, selectedPool)
-        .listen((UserModel updatedUser) async {
-      if (_user.photoURL != updatedUser.photoURL) {
-        _user.photoURL = updatedUser.photoURL;
-        await _user.loadPhotoFromURL(updatedUser.photoURL);
-      }
+    DataService.updatedUserModel(user, selectedPool).listen(onUpdatedUserModel);
+  }
 
-      _user = updatedUser;
+// TODO: This function will need to decide which change warrants [notifyListeners()]
+  void onUpdatedUserModel(UserModel updatedUser) async {
+    if (_user.uid != updatedUser.uid) {
+      poolState = PoolState.notstarted;
+      selectedPool = null;
+      availablePools = updatedUser.membership;
+      poolState =
+          availablePools.isEmpty ? PoolState.nopools : PoolState.retrieved;
+    }
 
-      notifyListeners();
-    });
+    _user = updatedUser;
+
+    notifyListeners();
   }
 }
